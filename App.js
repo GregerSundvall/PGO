5af09e69e67d9b60ad0b31e753386af58d2a1333
GregerSundvall <71319618+GregerSundvall@users.noreply.github.com>
2021-08-27 16:17:57 +0200

    context set up

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Game from "./components/Game";
+import MyContext from "./MyContext";
+import uniqueID from "react-native-web/dist/exports/StyleSheet/ReactNativePropRegistry";
+
 
 export default function App() {
+
+    const [pokemon, setPokemon] = useState([
+        {nr:1, name:'Bulbasaur', id: uniqueID},
+        {nr:3, name:'Charizard', id: uniqueID},
+        {nr:5, name:'Lucario', id: uniqueID},
+        {nr:4, name:'Pikachu', id: uniqueID},
+        {nr:7, name:'Snorlax', id: uniqueID},
+        {nr:10, name:'Blissey', id: uniqueID},
+    ])
+    
   return (
-      <View style={styles.container}>
-        <Game style={styles.container} />
-      </View>
+      <>
+          <MyContext.Provider value={{pokemon: pokemon}}>
+              <Game/>
+          </MyContext.Provider>
+      </>
+      
+      
   );
 }
 
